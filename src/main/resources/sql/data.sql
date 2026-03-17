7e55d40d086b04118dc180adbe6c8e260dfb579e
kasper <kasperj2016@gmail.com>
2026-03-17 10:37:53 +0100

    data til test

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,13 @@
+use touristguide;
+
+INSERT INTO location (city_name) values
+    ('Copenhagen V');
+
+INSERT INTO attractions (attraction_name, description, city_name) VALUES
+    ('Tivoli Gardens', 'Historic amusement park and pleasure garden opened in 1843. Visitors can enjoy roller coasters, gardens, restaurants, concerts and seasonal events like Christmas and Halloween markets.','Copenhagen V');
+
+INSERT INTO tag (tag) VALUES
+    ('Amusement park');
+
+INSERT INTO attraction_tag (attraction_name, tag) VALUES
+                                                      ('Tivoli Gardens'), ('Amusement park');
